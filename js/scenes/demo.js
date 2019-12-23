3dd92b9844d56832a073a2e34016ec4dcf7a3fa0
jjcapellan <soycape@hotmail.com>
2019-12-23 23:13:23 +0100

    Format

--- js/scenes/demo.js
+++ js/scenes/demo.js
@@ -4,9 +4,9 @@ class Demo extends Phaser.Scene {
   }
 
   create(data) {
-    const TILESIZE = data.tilesize;    
-    const vTiles = Math.floor(((this.game.config.height/TILESIZE) - 1));
-    const hTiles = Math.floor(((this.game.config.width/TILESIZE) - 1));
+    const TILESIZE = data.tilesize;
+    const vTiles = Math.floor(((this.game.config.height / TILESIZE) - 1));
+    const hTiles = Math.floor(((this.game.config.width / TILESIZE) - 1));
     const mapHeight = Math.floor((vTiles - 1) / 2);
     const mapWidth = Math.floor((hTiles - 1) / 2);
     // New Maze object
@@ -17,43 +17,43 @@ class Demo extends Phaser.Scene {
     mymaze.gateway(mapWidth - 1, mapHeight - 3);
     // Generates the maze map
     const mazeMap = mymaze.tiles();
-    // Center the map on screen
+    // Center map on screen
     const x = Math.round((this.game.config.width - TILESIZE * hTiles) / 2);
     const y = Math.round((this.game.config.height - TILESIZE * vTiles) / 2);
     // Display the maze map
     this.renderTiles(x, y, mazeMap, TILESIZE);
   }
 
-  swapZeros(arr){
-    arr.forEach((row,i) => {
-      row.forEach((v,i,a)=>{
+  swapZeros(arr) {
+    arr.forEach((row, i) => {
+      row.forEach((v, i, a) => {
         a[i] = a[i] ? 0 : 1;
-      });      
+      });
     });
   }
-  
 
-  renderTiles(x, y, maze, tilesize){
+
+  renderTiles(x, y, maze, tilesize) {
     const width = tilesize * maze[0].length;
     const height = tilesize * maze.length;
 
     // Walls    
     let wallsMap = this.make.tilemap({ data: maze, tileWidth: 50, tileHeight: 50 });
     let wallTile = wallsMap.addTilesetImage('wall');
-    let wallsLayer = wallsMap.createStaticLayer(0, wallTile, x , y);
-    wallsLayer.setDisplaySize(width,height);
+    let wallsLayer = wallsMap.createStaticLayer(0, wallTile, x, y);
+    wallsLayer.setDisplaySize(width, height);
 
     // Floor 
     this.swapZeros(maze); // swaps 0 - 1
     let map = this.make.tilemap({ data: maze, tileWidth: 50, tileHeight: 50 });
     let floorTile = map.addTilesetImage('floor');
     let floorLayer = map.createStaticLayer(0, floorTile, x, y);
-    floorLayer.setDisplaySize(width, height);    
-    
+    floorLayer.setDisplaySize(width, height);
+
     // Shadows
     const offset = 0.2 * tilesize; // 20% of tile
     let rt = this.add.renderTexture(x + offset, y + offset, width, height);
-    rt.draw(wallsLayer,0,0);
+    rt.draw(wallsLayer, 0, 0);
     rt.setAlpha(0.4);
     rt.setTint(0);
 
